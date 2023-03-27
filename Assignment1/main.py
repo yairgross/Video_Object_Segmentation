fe4455f5319d9e393af8aebe4bcf6fc61aa46e2c
yairg <yairgross12@gmail.com>
2023-03-27 19:37:20 +0300

    Implemented dc-dp means and its test. Need to fix dc-dp means function

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -4,7 +4,44 @@ import matplotlib.pyplot as plt
 import sklearn
 import sklearn.datasets as ds
 
-def dcdpmeans()
+def converged(clusters):
+    return False
+
+
+def dcdpmeans(x, l, t):
+    k = 1
+    N, d = np.shape(x)
+    centroids = [np.sum(x, axis=0) / N]
+    z = np.ones(N)
+    for step in range(t):
+        print(step)
+    #while not converged(): # TODO: implement
+        sizes = np.zeros(k)
+        sums = [np.zeros(d) for j in range(k)]
+        jmax = -1
+        dmax = -1
+        for i in range(N):
+            min_centroid_dis = math.inf
+            for centroid, j in zip(centroids, range(k)):
+                dis = np.linalg.norm(x[i] - centroid)
+                if dis < min_centroid_dis:
+                    min_dis = dis
+                    z[i] = j
+            if min_centroid_dis > dmax:
+                jmax = i
+                dmax = min_centroid_dis
+        if dmax > l:
+            k += 1
+            centroids.append(x[jmax])
+            z[jmax] = k-1
+        for m in range(k):
+            sum = np.zeros(d)
+            indecies = np.array(np.where(z == m))
+            size = len(indecies)
+            for index in indecies[0]:
+                sum += x[index]
+            centroids[m] = sum / size
+    return np.reshape(z, (z.shape[0], 1))
 
 
 def kmeans(X, k, t):
@@ -51,8 +88,19 @@ def test_kmeans():
         plt.show()
 
 
-
+def test_dcdpmeans():
+    # Generating data
+    N = 1000
+    x,y = ds.make_blobs(n_samples=N, n_features=2, centers=5, random_state=42)
+    ls = [1, 2, 5, 10]
+    for l in ls:
+        clusters = dcdpmeans(x, l, 400)
+        # Generating k random colors for each cluster
+        title = f"Test of DC-DP means implementation with lambda={l}"
+        plt.title(title)
+        plt.scatter(x[:, 0], x[:, 1], c=clusters, s=1)
+        plt.show()
 
 
 if __name__ == '__main__':
-    test_kmeans()
+    test_dcdpmeans()
