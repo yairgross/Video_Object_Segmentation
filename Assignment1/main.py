79942cae54d2e138c4f5afcbd8bc95d84684b894
yairg <yairgross12@gmail.com>
2023-04-27 16:03:18 +0300

    HW Assignment submitted

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -12,7 +12,6 @@ def kmeans(X, k):
     """
     :param X: numpy array of size (N, d) containing the samples
     :param k: the number of clusters
-    :param t: the number of iterations to run
     :return: a column vector of length N, where C(i) ∈ {1, . . . , k} is the identity of the cluster in which x_i has been assigned;
              an array containing the centroids
     """
@@ -21,7 +20,10 @@ def kmeans(X, k):
     centroids = X[np.random.choice(X.shape[0], size=k, replace=False)]
     clustering = np.zeros(N)
     oldc = np.ones(N)
+    step = 0
     while not np.array_equal(clustering, oldc):
+        # print(step)
+        step += 1
         oldc = np.copy(clustering)
         sizes = np.zeros(k)
         sums = [np.zeros(d) for j in range(k)]
@@ -41,9 +43,8 @@ def kmeans(X, k):
 
 def dcdpmeans(x, l):
     """
-        :param X: numpy array of size (N, d) containing the samples
+        :param x: numpy array of size (N, d) containing the samples
         :param l: lambda - the distance parameter to identify need of initializing a new cluster
-        :param t: the number of iterations to run
         :return: a column vector of length N, where C(i) ∈ {1, . . . , k} is the identity of the cluster in which x_i has been assigned;
                  an array containing the centroids
         """
@@ -52,8 +53,11 @@ def dcdpmeans(x, l):
     centroids = [np.sum(x, axis=0) / N]
     clustering = np.zeros(N)
     oldc = np.ones(N)
+    step = 0
     # Testing convergence by comparing membership of examples to the clusters
     while not np.array_equal(oldc, clustering):
+        # print(step)
+        step += 1
         oldc = np.copy(clustering)
         jmax = -1
         dmax = -1
@@ -70,7 +74,7 @@ def dcdpmeans(x, l):
         if dmax > l:
             k += 1
             centroids.append(x[jmax])
-            clustering[jmax] = k-1
+            clustering[jmax] = k - 1
         for m in range(k):
             sum = np.zeros(d)
             indecies = np.array(np.where(clustering == m))
@@ -87,23 +91,13 @@ def test_kmeans():
     # Generating data
     N = 1000
     x, y = ds.make_blobs(n_samples=N, n_features=2, centers=5, shuffle=True)
-    ks = [1, 5, 20, 50, 100, 200, 500, 1000]
+    ks = [1, 3, 5, 10, 20, 50, 100, 200, 500, 1000]
     for k in ks:
-        # Create a list of equally distributed N hue (color) values using a linear space
-        hue_vals = np.linspace(0, 1, N)
-        # Create a list of colors using the hsv color space
-        colors = []
-        for val in hue_vals:
-            color = plt.cm.hsv(val)
-            colors.append(color)
-        # Create a Listed Color Map using the list of colors
-        color_map = ListedColormap(colors)
-
         clustering, centroids = kmeans(x, k)
-        # Generating k random colors for each cluster
+
         title = f"Test of k means implementation with k={k}"
         plt.title(title)
-        plt.scatter(x[:, 0], x[:, 1], c=clustering, s=1, cmap=color_map)
+        plt.scatter(x[:, 0], x[:, 1], c=clustering, s=1, cmap="gist_rainbow")
         plt.show()
 
 
@@ -113,53 +107,54 @@ def test_dcdpmeans():
     x, y = ds.make_blobs(n_samples=N, n_features=2, centers=5, shuffle=True)
     ls = [1, 2, 3, 5, 10, 20]
     for l in ls:
-        clusters = dcdpmeans(x, l, 400)
-        # Generating k random colors for each cluster
+        clustering = dcdpmeans(x, l)
+
         title = f"Test of DC-DP means implementation with lambda={l}"
         plt.title(title)
-        plt.scatter(x[:, 0], x[:, 1], c=clustering, s=1, cmap=color_map)
+        plt.scatter(x[:, 0], x[:, 1], c=clustering[0], s=1, cmap="gist_rainbow")
         plt.show()
 
 
 # Task 3
 
 def mandrill_kmeans():
-    image = Image.open('mandrills/mandrill3.jpg')
+    image = Image.open('mandrill.jpg')
     image.show()
     img_array = np.array(image)
     rows, cols, rgb = img_array.shape
-    x = img_array.reshape(rows*cols, rgb)
-    ks = [10, 50, 100, 200, 500, 1000]
+    x = img_array.reshape(rows * cols, rgb)
+    ks = [2, 3, 5, 10, 30, 50, 100]
     for k in ks:
         clusters, centroids = kmeans(x, k)
         # Coloring each pixel in img_array by their corresponding centroid
         new_img_array = np.copy(img_array)
         for i in range(rows):
             for j in range(cols):
-                new_img_array[i][j] = centroids[int(clusters[i*cols + j])]
+                new_img_array[i][j] = centroids[int(clusters[i * cols + j])]
         new_img = Image.fromarray(new_img_array)
         new_img.show()
 
 
 def mandrill_dcdpmeans():
-    image = Image.open('mandrills/mandrill2.jpg')
+    image = Image.open('mandrill.jpg')
     image.show()
     img_array = np.array(image)
     rows, cols, rgb = img_array.shape
-    x = img_array.reshape(rows*cols, rgb)
-    print(x.shape)
-    ls = [50, 100, 150, 200, 250, 300]
+    x = img_array.reshape(rows * cols, rgb)
+    ls = [250, 200, 150, 100, 50]
     for l in ls:
         clustering, centroids = dcdpmeans(x, l)
         # Coloring each pixel in img_array by their corresponding centroid
         new_img_array = np.copy(img_array)
         for i in range(rows):
             for j in range(cols):
-                new_img_array[i][j] = centroids[int(clustering[i*cols + j])]
+                new_img_array[i][j] = centroids[int(clustering[i * cols + j])]
         new_img = Image.fromarray(new_img_array)
         new_img.show()
 
 
-
 if __name__ == '__main__':
-    mandrill_kmeans()
+    test_kmeans()
+    # test_dcdpmeans()
+    # mandrill_kmeans()
+    #mandrill_dcdpmeans()
